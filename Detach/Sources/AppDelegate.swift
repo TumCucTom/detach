bce67ef5f433999b67c3ed8fc898675eac4e9b6f
Thomas Bale <tokbale@outlook.com>
2026-02-01 17:50:37 +0000

    perf: Lazy load Assets
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -4,7 +4,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+        return false
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
