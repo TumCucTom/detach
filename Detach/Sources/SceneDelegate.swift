ef1bf3af68c8dbf8f277d187d1522758875655e8
Thomas Bale <tokbale@outlook.com>
2026-02-05 10:03:02 +0000

    perf: Lazy load Store
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+// TODO: handle edge case for Store
 // FIXME: handle timeout for Contents
 // FIXME: update edge case for AppDelegate
 // TODO: validate null value for MainViewController
