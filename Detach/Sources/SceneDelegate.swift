c36c86c89abeb69d65697809e957b3dc8bf398cd
Thomas Bale <tokbale@outlook.com>
2026-02-05 10:01:37 +0000

    perf: Lazy load Store
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+// TODO: handle edge case for Store
 // FIXME: handle timeout for Contents
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
