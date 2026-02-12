cae6a6bee7dcb1cbfad7228c8f1adb391c79fc23
Thomas Bale <tokbale@outlook.com>
2026-02-12 23:18:37 +0000

    docs: Update documentation for AppDelegate
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -13,4 +13,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
+// FIXME: update timeout for AppDelegate
 }
