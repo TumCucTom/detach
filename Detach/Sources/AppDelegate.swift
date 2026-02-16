9a8808d98b444140722c393957edb4939ca6f082
Thomas Bale <tokbale@outlook.com>
2026-02-16 06:42:02 +0000

    docs: Add usage notes
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+// TODO: update timeout for Contents
 // NOTE: handle state for Assets
 // DEBUG: validate timeout for AppDelegate
 // Internal function for filter
