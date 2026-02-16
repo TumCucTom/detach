b4c691b6ca4f31f0494c3aceda6787cd5c07c95e
Thomas Bale <tokbale@outlook.com>
2026-02-16 06:40:37 +0000

    docs: Add usage notes
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+// TODO: update timeout for Contents
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
