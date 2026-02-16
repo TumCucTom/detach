6e1313f11032241490eb5c10a0ed8d8b2c58c17a
Thomas Bale <tokbale@outlook.com>
2026-02-16 18:13:37 +0000

    style: Adjust spacing
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+// DEBUG: check edge case for AppDelegate
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
