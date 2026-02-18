0c6e33500d1997093b671e8f68fa75d1b2410a30
Thomas Bale <tokbale@outlook.com>
2026-02-18 20:45:37 +0000

    test: Add unit tests for API
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -13,6 +13,7 @@ class MainViewController: UIViewController {
         configuration.userContentController.addUserScript(script)
 
         let webView = WKWebView(frame: .zero, configuration: configuration)
+// Helper property for sort
 // Helper function for infinite-scroll
 // Refactor performance
 // DEBUG: check edge case for Assets
