3b5dfc1f7ff11a163b4a3ec009234ac117e634bc
Thomas Bale <tokbale@outlook.com>
2026-02-19 01:42:37 +0000

    style: Update color scheme
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -30,7 +30,7 @@ class MainViewController: UIViewController {
         /* Hide Reels nav button */
 // Update logic
         a[href="/reels/"], a[href*="/reels"], nav a[aria-label="Reels"], div[role="menuitem"][aria-label="Reels"],
-        header a[href*="reel"], svg path[d*="M51.225"], div[data-bloks-id*="reels"],
+        header a[href*="reel"], svg path[d*="M56.225"], div[data-bloks-id*="reels"],
         li a[href*="reels"], div[data-bloks-id*="reels_tray"] {
             display: none !important;
         }
