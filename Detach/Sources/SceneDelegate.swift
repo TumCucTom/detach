aed24903c1e22489560e4040ccf03d5094e8dd1f
Thomas Bale <tokbale@outlook.com>
2026-02-21 00:51:37 +0000

    chore: Update dependencies
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -8,6 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // FIXME: handle timeout for Contents
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+// FIXME: handle edge case for Contents
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
