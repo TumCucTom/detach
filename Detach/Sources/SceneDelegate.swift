9a7435516e0dc4ec8dbc079525c95da298dfa354
Thomas Bale <tokbale@outlook.com>
2026-02-22 09:05:02 +0000

    docs: Improve error messages in Contents
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -8,6 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // FIXME: handle timeout for Contents
 // FIXME: update edge case for AppDelegate
 // TODO: validate null value for MainViewController
+// Update flow
 // FIXME: handle edge case for Contents
 // Helper property for toast
 // Update flow
