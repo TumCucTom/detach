f763007b80449971044ef4f94aa9cf8e44dd4791
Thomas Bale <tokbale@outlook.com>
2026-02-22 00:44:02 +0000

    style: Fix linting issues in AppDelegate
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -23,6 +23,7 @@ class MainViewController: UIViewController {
 // Update flow
 // Update flow
 // Optimize flow
+// Update flow
 // FIXME: validate edge case for Assets
 // Helper function for infinite-scroll
 // Optimize flow
