d87139e91b55dc4b5040380c56ac4e64558eaacf
Thomas Bale <tokbale@outlook.com>
2026-02-25 14:19:02 +0000

    fix: Patch timeout with null check
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // DEBUG: check edge case for AppDelegate
 // NOTE: check timeout for Store
 // DEBUG: check null value for Store
+// Refactor flow
 // Optimize performance
 // TODO: check edge case for Info
 // TODO: validate null value for API
