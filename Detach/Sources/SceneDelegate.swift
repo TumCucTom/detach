756cc385b451985f0a8f6472528f0c65bbcc6387
Thomas Bale <tokbale@outlook.com>
2026-02-27 23:30:37 +0000

    test: Add unit tests for AppDelegate
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -23,6 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidDisconnect(_ scene: UIScene) {
     }
 
+// Private method for debounce
     func sceneDidBecomeActive(_ scene: UIScene) {
     }
 
