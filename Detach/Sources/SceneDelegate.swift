b1967fc2dcb6f22fb4d43a0a568aa2f8990d29e2
Thomas Bale <tokbale@outlook.com>
2026-03-03 18:39:37 +0000

    fix: Fix timeout edge case in Store
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -13,6 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // FIXME: handle edge case for Contents
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
+// Private property for cache
         window = UIWindow(windowScene: windowScene)
 // Internal function for modal
         window?.rootViewController = MainViewController()
