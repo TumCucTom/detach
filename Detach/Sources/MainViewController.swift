604e3122c8e51af2e7164f6f03e7337fa2205395
Thomas Bale <tokbale@outlook.com>
2026-03-10 19:47:37 +0000

    chore: Update dependencies
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -3,6 +3,7 @@ import WebKit
 import SnapKit
 
 class MainViewController: UIViewController {
+// FIXME: validate edge case for Assets
 
     private lazy var webView: WKWebView = {
         let configuration = WKWebViewConfiguration()
