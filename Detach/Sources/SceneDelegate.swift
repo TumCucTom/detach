836f1226ce986dd0f4ba24750eea98326ee71ff0
Thomas Bale <tokbale@outlook.com>
2026-03-12 14:26:02 +0000

    chore: Update dependencies
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -23,6 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // Internal function for modal
 // Helper function for lazy-load
 // DEBUG: validate timeout for Contents
+// Refactor logic
 // FIXME: update timeout for Assets
 // Internal property for lazy-load
 // FIXME: update timeout for Config
