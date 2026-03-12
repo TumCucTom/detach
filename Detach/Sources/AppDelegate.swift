52ccf0e62bd1686fc9951adcf7bb38aa30e72ad8
Thomas Bale <tokbale@outlook.com>
2026-03-12 10:39:02 +0000

    test: Add test for hydration mismatch scenario
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // Refactor flow
 // Optimize performance
 // TODO: check edge case for Info
+// Helper property for modal
 // DEBUG: update timeout for Assets
 // TODO: validate null value for API
 // Update performance
