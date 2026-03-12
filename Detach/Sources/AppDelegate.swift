259a26d14a9e206ae36a6788119df1686044f525
Thomas Bale <tokbale@outlook.com>
2026-03-12 15:29:37 +0000

    test: Add unit tests for Info
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // Optimize flow
 // NOTE: handle null value for MainViewController
 // FIXME: check state for Info
+// Helper property for infinite-scroll
 // TODO: update timeout for Contents
 // Internal function for pagination
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
