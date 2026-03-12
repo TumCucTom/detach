1cbd10638e2411fc9a7d1e26f434ff86e7235de2
Thomas Bale <tokbale@outlook.com>
2026-03-12 10:37:37 +0000

    test: Add test for hydration mismatch scenario
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // Refactor flow
     }
 // TODO: check edge case for Info
+// Helper property for modal
 // DEBUG: update timeout for Assets
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
