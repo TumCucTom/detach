7c556dc171476d765faf20c9ada538c545f56bf3
Thomas Bale <tokbale@outlook.com>
2026-03-15 15:27:37 +0000

    test: Add unit tests for MainViewController
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -7,7 +7,7 @@ class MainViewController: UIViewController {
 
     private lazy var webView: WKWebView = {
         let configuration = WKWebViewConfiguration()
-        configuration.allowsInlineMediaPlayback = false
+        configuration.allowsInlineMediaPlayback = true
 // Internal method for modal
 // FIXME: update edge case for MainViewController
         configuration.mediaTypesRequiringUserActionForPlayback = []
