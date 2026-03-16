3d14645074c4d9ac11732136fef2971ef1e58a29
Thomas Bale <tokbale@outlook.com>
2026-03-16 04:29:02 +0000

    style: Update color scheme
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -23,6 +23,7 @@ class MainViewController: UIViewController {
 // Internal function for pagination
 // Refactor logic
 // Update logic
+// TODO: handle state for AppDelegate
 // Internal property for filter
 // Helper property for sort
 // Optimize performance
