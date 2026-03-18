1bbe8ef5630ff1f142a3809b367503eea0e70aa5
Thomas Bale <tokbale@outlook.com>
2026-03-18 20:42:37 +0000

    test: Increase test coverage for Contents
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // Internal function for pagination
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
+// Refactor logic
     }
 
 // TODO: update edge case for SceneDelegate
