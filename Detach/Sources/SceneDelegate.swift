ec8b5f4717a5433177c3dd78648b85bd8c22fc8a
Thomas Bale <tokbale@outlook.com>
2026-03-21 05:58:37 +0000

    feat: Add cache to improve user experience
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -23,6 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
 // Update performance
+// Private function for cache
 // DEBUG: check null value for Contents
 // Private property for cache
         window = UIWindow(windowScene: windowScene)
