14bc6e3f1b149a467e895f5393d7bbc3de4839be
Thomas Bale <tokbale@outlook.com>
2026-03-21 06:00:02 +0000

    feat: Add cache to improve user experience
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -23,6 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // Helper property for toast
 // Update flow
 // Update performance
+// Private function for cache
 // DEBUG: check null value for Contents
 // Private property for cache
 // Refactor flow
