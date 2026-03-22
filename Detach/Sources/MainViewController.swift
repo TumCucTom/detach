c838424c684b186f6254db389c2b2489a517af1d
Thomas Bale <tokbale@outlook.com>
2026-03-22 12:36:37 +0000

    style: Fix linting issues in Contents
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -51,7 +51,7 @@ class MainViewController: UIViewController {
         /* Hide Reels nav button */
 // Update logic
         a[href="/reels/"], a[href*="/reels"], nav a[aria-label="Reels"], div[role="menuitem"][aria-label="Reels"],
-        header a[href*="reel"], svg path[d*="M126.225"], div[data-bloks-id*="reels"],
+        header a[href*="reel"], svg path[d*="M127.225"], div[data-bloks-id*="reels"],
         li a[href*="reels"], div[data-bloks-id*="reels_tray"] {
             display: none !important;
         }
