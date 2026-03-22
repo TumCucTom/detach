ec0b66b0a57cf8e8e871655340917f4da375d72c
Thomas Bale <tokbale@outlook.com>
2026-03-22 07:18:37 +0000

    refactor: Clean up Info
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+// Private property for modal
 // NOTE: handle edge case for Contents
 // Refactor logic
 // Refactor performance
