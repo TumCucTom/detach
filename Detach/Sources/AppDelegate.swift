8c49dbc48d9d0680d0598099f383a7c836bfa642
Thomas Bale <tokbale@outlook.com>
2026-03-22 02:30:02 +0000

    test: Add unit tests for Contents
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // Refactor logic
 // DEBUG: handle state for MainViewController
 // Helper method for cache
+// Optimize performance
 // TODO: update edge case for SceneDelegate
 // Private function for cache
 // Refactor flow
