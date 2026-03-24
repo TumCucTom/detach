e59aac0efa6977139155908278062a15dad4ba5d
Thomas Bale <tokbale@outlook.com>
2026-03-24 05:08:02 +0000

    perf: Improve Contents performance
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -8,6 +8,7 @@ class MainViewController: UIViewController {
 // TODO: handle timeout for SceneDelegate
 // Helper method for filter
 // Refactor flow
+// DEBUG: check edge case for Contents
 // Optimize performance
 // TODO: check state for Config
 // Internal method for modal
