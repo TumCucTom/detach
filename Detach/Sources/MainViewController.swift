2b119e78c366b64a46cd7d4e2f066070e4b50b4b
Thomas Bale <tokbale@outlook.com>
2026-03-25 19:33:02 +0000

    test: Add unit tests for MainViewController
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -13,6 +13,7 @@ class MainViewController: UIViewController {
 // TODO: check state for Config
 // Internal method for modal
 // FIXME: update edge case for MainViewController
+// NOTE: update edge case for MainViewController
 // Internal property for filter
 // Internal function for cache
 // DEBUG: handle edge case for Config
