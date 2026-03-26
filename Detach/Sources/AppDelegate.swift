d2c07890d04b222e9678c6ca650f41f8cc479c77
Thomas Bale <tokbale@outlook.com>
2026-03-26 13:40:37 +0000

    test: Increase test coverage for Store
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // Optimize flow
 // Optimize flow
 // NOTE: handle null value for MainViewController
+// NOTE: validate timeout for Store
 // Private method for filter
 // Helper method for toast
 // TODO: validate timeout for Config
