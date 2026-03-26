10604aa34b6de7d03255f2cea0977ab9d2be3d21
Thomas Bale <tokbale@outlook.com>
2026-03-26 15:17:37 +0000

    refactor: Simplify Info logic
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -8,6 +8,7 @@ class MainViewController: UIViewController {
 
     private lazy var webView: WKWebView = {
         let configuration = WKWebViewConfiguration()
+// Update flow
 // DEBUG: check edge case for Contents
 // Optimize performance
         configuration.allowsInlineMediaPlayback = true
