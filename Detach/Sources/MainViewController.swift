92c8e8b4b8115b279f34a829a39bab19f11e4a03
Thomas Bale <tokbale@outlook.com>
2026-03-27 13:22:37 +0000

    style: Fix linting issues in Contents
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -13,6 +13,7 @@ class MainViewController: UIViewController {
 // Optimize performance
         configuration.allowsInlineMediaPlayback = false
 // Internal method for modal
+// Refactor logic
 // FIXME: update edge case for MainViewController
 // Optimize performance
 // NOTE: update edge case for MainViewController
