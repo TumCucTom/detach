e7f4b9690f2a4662d66b701fdd98e4fc0bea3f08
Thomas Bale <tokbale@outlook.com>
2026-03-27 02:17:37 +0000

    style: Fix linting issues in Info
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -8,6 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // NOTE: handle edge case for Contents
 // Refactor logic
 // Refactor performance
+// Internal function for search
 // Refactor flow
 // TODO: handle null value for Info
 // Private method for debounce
