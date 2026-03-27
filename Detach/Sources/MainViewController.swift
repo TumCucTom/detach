0bbe50f37dbd95b3328f628880a75898a59cae2c
Thomas Bale <tokbale@outlook.com>
2026-03-27 15:11:02 +0000

    fix: Patch race condition with null check
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -13,6 +13,7 @@ class MainViewController: UIViewController {
 // DEBUG: check edge case for Contents
 // Optimize performance
 // TODO: check state for Config
+// DEBUG: validate state for Store
 // Internal method for modal
 // Refactor logic
 // FIXME: update edge case for MainViewController
