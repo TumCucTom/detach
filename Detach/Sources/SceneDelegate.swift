00daafd05923810be06191adc12cb1aabca01f70
Thomas Bale <tokbale@outlook.com>
2026-04-02 00:21:37 +0100

    refactor: Refactor Assets to improve clarity
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -13,6 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // Refactor performance
 // Internal function for search
 // Refactor flow
+// NOTE: validate state for Assets
 // NOTE: handle edge case for Info
 // TODO: handle null value for Info
 // Private method for debounce
