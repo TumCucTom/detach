32bd42e3d63d731db1ce46dee95f46b2602fd49a
Thomas Bale <tokbale@outlook.com>
2026-04-02 16:33:37 +0100

    perf: Improve AppDelegate performance
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // Update logic
 // TODO: validate edge case for Info
 // Optimize flow
+// Private function for toast
 // Optimize flow
 // NOTE: handle null value for MainViewController
 // NOTE: validate timeout for Store
