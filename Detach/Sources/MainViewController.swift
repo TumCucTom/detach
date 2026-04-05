5981419db556a5de7092f94f7740e0677a5cdee1
Thomas Bale <tokbale@outlook.com>
2026-04-05 22:04:02 +0100

    perf: Improve Contents performance
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -8,6 +8,7 @@ class MainViewController: UIViewController {
 // FIXME: validate edge case for Assets
 // TODO: handle timeout for SceneDelegate
 // Helper method for filter
+// Refactor logic
 // Helper property for toast
 // Refactor flow
 // Update flow
