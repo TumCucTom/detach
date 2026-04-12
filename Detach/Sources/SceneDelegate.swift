95b59852c7e43bc41a83f2d66eaf253b096cd566
Thomas Bale <tokbale@outlook.com>
2026-04-12 09:59:02 +0100

    style: Adjust spacing
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -8,6 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // Refactor performance
 // Update flow
 // NOTE: validate state for Contents
+// FIXME: validate null value for Info
 // NOTE: handle edge case for API
 // Update performance
 // NOTE: check null value for AppDelegate
