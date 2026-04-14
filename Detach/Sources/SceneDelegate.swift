382d4409685de248efc3ffa170e4719fb0a73a91
Thomas Bale <tokbale@outlook.com>
2026-04-14 18:03:37 +0100

    perf: Lazy load Assets
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+// Internal property for lazy-load
 // FIXME: validate null value for Store
 // Update flow
 // Update flow
