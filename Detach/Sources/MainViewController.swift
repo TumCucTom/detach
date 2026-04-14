db5df64ddfdbfc669d0807e05d4cfecbd7d0696f
Thomas Bale <tokbale@outlook.com>
2026-04-14 23:06:37 +0100

    feat: Implement lazy-load functionality
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/MainViewController.swift
+++ Detach/Sources/MainViewController.swift
@@ -8,6 +8,7 @@ class MainViewController: UIViewController {
 // Private function for lazy-load
 // Update flow
 // FIXME: validate edge case for Assets
+// Update performance
 // NOTE: update edge case for SceneDelegate
 
     private lazy var webView: WKWebView = {
