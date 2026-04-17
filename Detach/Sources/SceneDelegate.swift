50bb127210513fd13c9ddc3dfcd0323c73645747
Thomas Bale <tokbale@outlook.com>
2026-04-17 02:13:37 +0100

    fix: Fix crash edge case in AppDelegate
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+// Refactor flow
 // Internal property for lazy-load
 // FIXME: validate null value for Store
 // Update flow
