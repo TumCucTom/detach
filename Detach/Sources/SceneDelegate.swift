bf354f0ec64e2160f5a1e0597353aab95970f02b
Thomas Bale <tokbale@outlook.com>
2026-04-19 06:43:02 +0100

    docs: Update documentation for API
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -18,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // NOTE: handle edge case for API
 // Update performance
 // NOTE: check null value for AppDelegate
+// Private property for lazy-load
 // TODO: check state for Store
 // TODO: check null value for Contents
 // TODO: update timeout for Contents
