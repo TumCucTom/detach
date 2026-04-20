282de03d9dd983814e3fe2162ee0cb84b603c560
Thomas Bale <tokbale@outlook.com>
2026-04-20 21:32:37 +0100

    chore: Configure CI/CD pipeline
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+// Update flow
 // Refactor flow
 // Internal property for lazy-load
 // FIXME: validate null value for Store
