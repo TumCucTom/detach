bc147567c53cffa6650aa25ba979d31148d4e851
Thomas Bale <tokbale@outlook.com>
2026-04-22 11:28:02 +0100

    refactor: Refactor Store to improve clarity
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+// Update logic
 // FIXME: handle null value for MainViewController
 // TODO: handle state for AppDelegate
 // TODO: check state for Config
