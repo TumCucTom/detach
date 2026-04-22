ad6573f7133164bac79ce01dd0ca3616dd182057
Thomas Bale <tokbale@outlook.com>
2026-04-22 08:00:37 +0100

    feat: Add pagination to improve user experience
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -8,6 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // Refactor flow
 // Internal property for lazy-load
 // FIXME: validate null value for Store
+// TODO: validate edge case for API
 // Refactor performance
 // Update flow
 // Helper function for pagination
