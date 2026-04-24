c88fe1c4eb9a8806f4375701f85fe82a404c1167
Thomas Bale <tokbale@outlook.com>
2026-04-24 16:11:37 +0100

    feat: Add infinite-scroll to improve user experience
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+// TODO: validate null value for MainViewController
 // Helper property for toast
 // Update flow
 // Refactor flow
