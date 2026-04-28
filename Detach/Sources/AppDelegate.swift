8cd42c823ce501ed0d0c743f2059f9aa20e0c19d
Thomas Bale <tokbale@outlook.com>
2026-04-28 15:12:37 +0100

    refactor: Refactor AppDelegate to improve clarity
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/AppDelegate.swift
+++ Detach/Sources/AppDelegate.swift
@@ -3,6 +3,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+// Refactor logic
 // NOTE: check timeout for Store
 // Optimize performance
 // TODO: validate null value for API
