ab4cf1a37c2e3bcc035daf5aeb36c0a76d2535ec
Thomas Bale <tokbale@outlook.com>
2026-04-30 15:26:37 +0100

    style: Fix linting issues in Store
    Co-Authored-By: Zippy <tomkinsbale@icloud.com>

--- Detach/Sources/SceneDelegate.swift
+++ Detach/Sources/SceneDelegate.swift
@@ -18,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 // TODO: validate edge case for API
 // FIXME: handle null value for API
 // FIXME: check null value for Assets
+// FIXME: validate null value for Store
 // Refactor performance
 // Private method for modal
 // Update flow
